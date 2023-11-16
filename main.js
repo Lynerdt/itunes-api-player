7a5272a9fdd8bb4f5ed4c0cbeae3defb340269d7
Lynerd <tilleryl10@gmail.com>
2023-11-15 21:53:03 -0500

    working and styled

--- main.js
+++ main.js
@@ -1,43 +1,60 @@
-const iTunesUrl = "https://itunes.apple.com/search?term=";
-const limit = "&limit=10";
-let searchForm = document.getElementById("searchForm");
-let searchTerm = document.getElementById("searchField");
-let button = document.getElementById("searchButton");
-let container = document.getElementById("container");
-let preview = document.getElementById("preview");
+const itunesUrl = "https://itunes.apple.com/search?term=";
+const previewUrl = "https://geo.itunes.apple.com/us/album/";
 
-searchForm.addEventListener("submit", (event) => {
+let inputForm = document.getElementById("inputForm");
+let inputField = document.getElementById("inputField");
+let results = document.getElementById("results");
+let button = document.getElementById("inputButton");
+let container = document.getElementById("container");
+let preview = document.getElementById("previewUrl");
+let songTile = document.getElementById("trackName");
+
+inputForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  let term = searchTerm.value;
-  console.log(term);
-  fetch(iTunesUrl + term + limit, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  })
+  let term = inputField.value;
+
+  document.addEventListener("autoPlay", (event) => {
+    const audio = document.getElementsByTagName("audio");
+  });
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  fetch(itunesUrl + term + "&media=music" + "&limit=22" + "&country=us")
     .then((response) => {
       if (response.status === 200) {
         return response.json();
       } else {
         let errorMsg = document.createElement("h2");
         errorMsg.innerText = "API call failed, please try again or reload page.";
-        container.appendChild(errorMsg);
+        results.appendChild(errorMsg);
       }
     })
     .then((parsedJsonResponse) => {
       console.log(parsedJsonResponse);
       const songs = parsedJsonResponse.results;
       return songs.map((results) => {
-        (preview.innerHTML = `
-      <figure>
-        <figcaption>Preview:</figcaption>
-        <audio controls src=""></audio>
-        </figure>`),
-          (container.innerHTML += `
-        <div class="songTile">
-        <img src=${results.artworkUrl100} />
-        <button class="preview">${results.trackName}</button>
-        <h3>${results.artistName}</h3>
-        </div>`);
+        const songCard = document.createElement("div"),
+          artist = document.createElement("h2"),
+          track = document.createElement("h2"),
+          img = document.createElement("img"),
+          playButton = document.createElement("button");
+        playButton.innerText = "Play Preview";
+
+        playButton.addEventListener("click", (event) => {
+          event.preventDefault();
+          preview.src = results.previewUrl;
+          songTitle.innerText += " " + results.trackName;
+        });
+        artist.innerHTML = results.artistName;
+        track.innerHTML = results.trackName;
+        img.src = results.artworkUrl100;
+
+        songCard.appendChild(img);
+        songCard.appendChild(artist);
+        songCard.appendChild(track);
+        songCard.appendChild(playButton);
+
+        container.appendChild(songCard);
       });
     });
 });
